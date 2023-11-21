32db3e705c2eb8068aec5b2cc82f131dbfe77e95
김민우 <mkw3295@gmail.com>
2023-11-21 09:54:33 +0900

    day01

---
+++ day01_java/src/ex02/dataType/Comment.java
@@ -0,0 +1,22 @@
+package ex02.dataType;
+
+public class Comment {
+	public static void main(String[] args) {
+		System.out.print("Hello JAVA!\n");		
+		System.out.println(100);
+		System.out.print("asdf\n");
+	}
+}
+
+// 실행 : Ctrl + F11
+
+// 한 줄 주석
+/* 
+  여러 줄 주석 
+ */
+
+// Ctrl + /
+/*
+ * Ctrl + Shift + /
+ * 
+ * */
